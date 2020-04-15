f0699ef901113173dc71388469140eb038d60b8a
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-04-15 10:41:43 -0300

    Adicionei novamente o Projeto

---
+++ ProjetoSeriesDeTv/GerenciamentoDasCategorias.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace ProjetoSeriesDeTv
+{
+    partial class frmGerenciamentoDasCategorias
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGerenciamentoDasCategorias));
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBoxEditarExcluirCategoria = new System.Windows.Forms.GroupBox();
+            this.btnLimparMostrarCategoria = new System.Windows.Forms.Button();
+            this.txtCodigoParaEditarCategoria = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnMostrarCategoriaParaEditar = new System.Windows.Forms.Button();
+            this.groupBoxAdicionarCategoria = new System.Windows.Forms.GroupBox();
+            this.btnSalvarCategoria = new System.Windows.Forms.Button();
+            this.btnExcluirCategoria = new System.Windows.Forms.Button();
+            this.txtNomeCategoria = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCodigoCategoria = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdicionarCategoria = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBoxEditarExcluirCategoria.SuspendLayout();
+            this.groupBoxAdicionarCategoria.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Calibri", 20F);
+            this.label8.Image = ((System.Drawing.Image)(resources.GetObject("label8.Image")));
+            this.label8.Location = new System.Drawing.Point(330, 64);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(518, 49);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Gerenciamento das Categorias";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(280, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(44, 35);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBoxEditarExcluirCategoria
+            // 
+            this.groupBoxEditarExcluirCategoria.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBoxEditarExcluirCategoria.BackgroundImage")));
+            this.groupBoxEditarExcluirCategoria.Controls.Add(this.btnLimparMostrarCategoria);
+            this.groupBoxEditarExcluirCategoria.Controls.Add(this.txtCodigoParaEditarCategoria);
+            this.groupBoxEditarExcluirCategoria.Controls.Add(this.label6);
+            this.groupBoxEditarExcluirCategoria.Controls.Add(this.btnMostrarCategoriaParaEditar);
+            this.groupBoxEditarExcluirCategoria.Font = new System.Drawing.Font("Calibri", 12F);
+            this.groupBoxEditarExcluirCategoria.Location = new System.Drawing.Point(571, 155);
+            this.groupBoxEditarExcluirCategoria.Name = "groupBoxEditarExcluirCategoria";
+            this.groupBoxEditarExcluirCategoria.Size = new System.Drawing.Size(477, 289);
+            this.groupBoxEditarExcluirCategoria.TabIndex = 22;
+            this.groupBoxEditarExcluirCategoria.TabStop = false;
+            this.groupBoxEditarExcluirCategoria.Text = "Edite ou Exclua suas categorias";
+            // 
+            // btnLimparMostrarCategoria
+            // 
+            this.btnLimparMostrarCategoria.Font = new System.Drawing.Font("Calibri", 12F);
+            this.btnLimparMostrarCategoria.Location = new System.Drawing.Point(237, 218);
+            this.btnLimparMostrarCategoria.Name = "btnLimparMostrarCategoria";
+            this.btnLimparMostrarCategoria.Size = new System.Drawing.Size(106, 40);
+            this.btnLimparMostrarCategoria.TabIndex = 6;
+            this.btnLimparMostrarCategoria.Text = "Limpar";
+            this.btnLimparMostrarCategoria.UseVisualStyleBackColor = true;
+            this.btnLimparMostrarCategoria.Click += new System.EventHandler(this.BtnLimparMostrarCategoria_Click);
+            // 
+            // txtCodigoParaEditarCategoria
+            // 
+            this.txtCodigoParaEditarCategoria.BackColor = System.Drawing.SystemColors.Info;
+            this.txtCodigoParaEditarCategoria.Location = new System.Drawing.Point(172, 149);
+            this.txtCodigoParaEditarCategoria.Name = "txtCodigoParaEditarCategoria";
+            this.txtCodigoParaEditarCategoria.Size = new System.Drawing.Size(106, 37);
+            this.txtCodigoParaEditarCategoria.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 12F);
+            this.label6.Image = ((System.Drawing.Image)(resources.GetObject("label6.Image")));
+            this.label6.Location = new System.Drawing.Point(19, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(442, 29);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Código da categoria que você deseja editar:";
+            // 
+            // btnMostrarCategoriaParaEditar
+            // 
+            this.btnMostrarCategoriaParaEditar.Font = new System.Drawing.Font("Calibri", 12F);
+            this.btnMostrarCategoriaParaEditar.Location = new System.Drawing.Point(114, 218);
+            this.btnMostrarCategoriaParaEditar.Name = "btnMostrarCategoriaParaEditar";
+            this.btnMostrarCategoriaParaEditar.Size = new System.Drawing.Size(106, 40);
+            this.btnMostrarCategoriaParaEditar.TabIndex = 5;
+            this.btnMostrarCategoriaParaEditar.Text = "Mostrar";
+            this.btnMostrarCategoriaParaEditar.UseVisualStyleBackColor = true;
+            this.btnMostrarCategoriaParaEditar.Click += new System.EventHandler(this.BtnMostrarCategoriaParaEditar_Click);
+            // 
+            // groupBoxAdicionarCategoria
+            // 
+            this.groupBoxAdicionarCategoria.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBoxAdicionarCategoria.BackgroundImage")));
+            this.groupBoxAdicionarCategoria.Controls.Add(this.btnSalvarCategoria);
+            this.groupBoxAdicionarCategoria.Controls.Add(this.btnExcluirCategoria);
+            this.groupBoxAdicionarCategoria.Controls.Add(this.txtNomeCategoria);
+            this.groupBoxAdicionarCategoria.Controls.Add(this.label2);
+            this.groupBoxAdicionarCategoria.Controls.Add(this.txtCodigoCategoria);
+            this.groupBoxAdicionarCategoria.Controls.Add(this.label1);
+            this.groupBoxAdicionarCategoria.Controls.Add(this.btnAdicionarCategoria);
+            this.groupBoxAdicionarCategoria.Font = new System.Drawing.Font("Calibri", 12F);
+            this.groupBoxAdicionarCategoria.Location = new System.Drawing.Point(45, 155);
+            this.groupBoxAdicionarCategoria.Name = "groupBoxAdicionarCategoria";
+            this.groupBoxAdicionarCategoria.Size = new System.Drawing.Size(477, 289);
+            this.groupBoxAdicionarCategoria.TabIndex = 25;
+            this.groupBoxAdicionarCategoria.TabStop = false;
+            this.groupBoxAdicionarCategoria.Text = "Adicione suas categorias";
+            // 
+            // btnSalvarCategoria
+            // 
+            this.btnSalvarCategoria.Font = new System.Drawing.Font("Calibri", 12F);
+            this.btnSalvarCategoria.Location = new System.Drawing.Point(171, 230);
+            this.btnSalvarCategoria.Name = "btnSalvarCategoria";
+            this.btnSalvarCategoria.Size = new System.Drawing.Size(120, 40);
+            this.btnSalvarCategoria.TabIndex = 2;
+            this.btnSalvarCategoria.Text = "Salvar";
+            this.btnSalvarCategoria.UseVisualStyleBackColor = true;
+            this.btnSalvarCategoria.Click += new System.EventHandler(this.BtnSalvarCategoria_Click);
+            // 
+            // btnExcluirCategoria
+            // 
+            this.btnExcluirCategoria.Font = new System.Drawing.Font("Calibri", 12F);
+            this.btnExcluirCategoria.Location = new System.Drawing.Point(306, 231);
+            this.btnExcluirCategoria.Name = "btnExcluirCategoria";
+            this.btnExcluirCategoria.Size = new System.Drawing.Size(122, 40);
+            this.btnExcluirCategoria.TabIndex = 3;
+            this.btnExcluirCategoria.Text = "Excluir";
+            this.btnExcluirCategoria.UseVisualStyleBackColor = true;
+            this.btnExcluirCategoria.Click += new System.EventHandler(this.BtnExcluirCategoria_Click);
+            // 
+            // txtNomeCategoria
+            // 
+            this.txtNomeCategoria.BackColor = System.Drawing.SystemColors.Info;
+            this.txtNomeCategoria.Location = new System.Drawing.Point(24, 169);
+            this.txtNomeCategoria.Name = "txtNomeCategoria";
+            this.txtNomeCategoria.Size = new System.Drawing.Size(413, 37);
+            this.txtNomeCategoria.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F);
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.Location = new System.Drawing.Point(19, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 29);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Categoria:";
+            // 
+            // txtCodigoCategoria
+            // 
+            this.txtCodigoCategoria.BackColor = System.Drawing.SystemColors.Info;
+            this.txtCodigoCategoria.Enabled = false;
+            this.txtCodigoCategoria.Location = new System.Drawing.Point(24, 103);
+            this.txtCodigoCategoria.Name = "txtCodigoCategoria";
+            this.txtCodigoCategoria.Size = new System.Drawing.Size(106, 37);
+            this.txtCodigoCategoria.TabIndex = 24;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F);
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(19, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 29);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Código:";
+            // 
+            // btnAdicionarCategoria
+            // 
+            this.btnAdicionarCategoria.Font = new System.Drawing.Font("Calibri", 12F);
+            this.btnAdicionarCategoria.Location = new System.Drawing.Point(27, 231);
+            this.btnAdicionarCategoria.Name = "btnAdicionarCategoria";
+            this.btnAdicionarCategoria.Size = new System.Drawing.Size(124, 40);
+            this.btnAdicionarCategoria.TabIndex = 1;
+            this.btnAdicionarCategoria.Text = "Adicionar";
+            this.btnAdicionarCategoria.UseVisualStyleBackColor = true;
+            this.btnAdicionarCategoria.Click += new System.EventHandler(this.BtnAdicionarCategoria_Click);
+            // 
+            // frmGerenciamentoDasCategorias
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 49F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1166, 520);
+            this.Controls.Add(this.groupBoxAdicionarCategoria);
+            this.Controls.Add(this.groupBoxEditarExcluirCategoria);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label8);
+            this.Font = new System.Drawing.Font("Calibri", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "frmGerenciamentoDasCategorias";
+            this.Text = "Gerenciamento das Categorias";
+            this.Load += new System.EventHandler(this.FrmGerenciamentoDasCategorias_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBoxEditarExcluirCategoria.ResumeLayout(false);
+            this.groupBoxEditarExcluirCategoria.PerformLayout();
+            this.groupBoxAdicionarCategoria.ResumeLayout(false);
+            this.groupBoxAdicionarCategoria.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBoxEditarExcluirCategoria;
+        private System.Windows.Forms.TextBox txtCodigoParaEditarCategoria;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnMostrarCategoriaParaEditar;
+        private System.Windows.Forms.GroupBox groupBoxAdicionarCategoria;
+        private System.Windows.Forms.Button btnSalvarCategoria;
+        private System.Windows.Forms.Button btnExcluirCategoria;
+        private System.Windows.Forms.TextBox txtNomeCategoria;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCodigoCategoria;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdicionarCategoria;
+        private System.Windows.Forms.Button btnLimparMostrarCategoria;
+    }
+}
